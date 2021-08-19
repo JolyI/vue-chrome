bce636f99697e17eeb5693fb7fdbbf311019e32d
DESKTOP-J26UKE9\zcsd <ah.yu@wisetimes.cn>
2021-08-19 19:45:44 +0800

    feat: 通信测试

--- src/chrome/content-script.js
+++ src/chrome/content-script.js
@@ -1,6 +1,21 @@
+console.log('xxxxxxxxxxxxx')
+var html = document.querySelectorAll('IMG')
+console.log(html)
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   // console.log(sender.tab ?"from a content script:" + sender.tab.url :"from the extension");
-  // if (request.cmd == 'test') alert(request.value)
-  console.log('接收到消息')
-  sendResponse('我收到了你的消息！')
+  if (request.cmd == 'test') { console.log('接收到消息') }
+  var html = document.querySelectorAll('IMG')
+  sendResponse(html)
+  sendIndex()
 })
+
+function sendIndex () {
+  var nodeList = document.querySelectorAll('IMG')
+  var arr = []
+  nodeList.forEach(item => {
+    arr.push(item.src)
+  })
+  chrome.runtime.sendMessage({ greeting: '你好，我是content-script呀，我主动发消息给后台！', arr }, function (response) {
+    console.log('收到来自后台的回复：' + response)
+  })
+}
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -11,6 +11,9 @@ export default {
       msg: 'this is popup page'
     }
   },
+  created () {
+    this.init()
+  },
   methods: {
     hanleClick () {
       // const bg = chrome.extension.getBackgroundPage()
@@ -24,6 +27,14 @@ export default {
         chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
           if (callback) callback(response)
         })
+      })
+    },
+    init () {
+      // 监听来自content-script的消息
+      chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        console.log('收到来自content-script的消息：')
+        console.log(request, sender, sendResponse)
+        sendResponse('我是后台，我已收到你的消息：' + JSON.stringify(request))
       })
     }
 
